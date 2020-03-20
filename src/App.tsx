27d6d901eef67ebdebcb5513b0447a2aa13c5022
Coleman Rose <coleman.rose@beamonpeople.se>
2020-03-20 16:32:32 +0100

    Further styling of homepage.

--- src/App.tsx
+++ src/App.tsx
@@ -8,17 +8,19 @@ function App() {
   return (
     <>
       <Header />
-      <div className={styles.blueBox}>
-      <a href="#music">music</a>
-      </div>
-      <div className={styles.redBox}>
-        <a href="#visuals">visuals</a>
-      </div>
-      <div className={styles.greenBox}>
-      <a href="#shop">shop</a>
-      </div>
-      <div className={styles.purpleBox}>
-      <a href="#about">about</a>
+      <div className={styles.centered}>
+        <div className={styles.blueBox}>
+        <a href="#music">music</a>
+        </div>
+        <div className={styles.redBox}>
+          <a href="#visuals">visuals</a>
+        </div>
+        <div className={styles.greenBox}>
+        <a href="#shop">shop</a>
+        </div>
+        <div className={styles.purpleBox}>
+        <a href="#about">about</a>
+        </div>
       </div>
       <Footer />
     </>
