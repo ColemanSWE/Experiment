b8f9eb8fc9c85b87cc25a1ec71d3690f121b1a0c
Coleman Rose <coleman.rose@beamonpeople.se>
2020-03-23 16:01:15 +0100

    Creating new views for about, shop, music, and visuals.

---
+++ src/Views/Visuals/Visuals.tsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import { Header } from './Components/Header'
+import { Footer } from './Components/Footer'
+import styles from './styles.module.scss'
+
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
