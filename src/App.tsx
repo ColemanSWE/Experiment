636f3d3cf88f6a7234cd1c8f5f46c5b43921175d
Coleman Rose <coleman.rose@beamonpeople.se>
2020-03-23 13:33:00 +0100

    New packages and starting to add routing to the homepage.

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,24 @@
-import React from 'react';
+import React from 'react'
 // import { NoiseGen } from './Instruments/NoiseGens/NoiseGens'
-import { Header } from './Components/Header';
-import { Footer } from './Components/Footer';
+import { Header } from './Components/Header'
+import { Footer } from './Components/Footer'
 import styles from './styles.module.scss'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom"
 
 function App() {
   return (
     <>
       <Header />
+      <Router>
       <div className={styles.grad}>
         <div className={styles.centered}>
           <div className={styles.blueBox}>
-            <a href="#music">music</a>
+            <Route href="#music">music</Route>
           </div>
           <div className={styles.redBox}>
             <a href="#visuals">visuals</a>
@@ -25,7 +32,7 @@ function App() {
         </div>
       </div>
       <Footer />
-      
+      </Router>
     </>
   );
 }
