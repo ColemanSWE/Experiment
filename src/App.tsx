a98f3dfb4dfb220b5b05a2ee38702754e069651e
Coleman Rose <coleman.rose@beamonpeople.se>
2020-04-07 13:20:55 +0200

    Finally figured out the routing.

--- src/App.tsx
+++ src/App.tsx
@@ -14,46 +14,47 @@ import {
 } from "react-router-dom"
 
 function App() {
-  return (
-    <>
-      <Header />
-      <Router>
-      <div className={styles.grad}>
-        <div className={styles.centered}>
-          <div className={styles.blueBox}>
-            <Link to="music">music</Link>
-          </div>
-          <div className={styles.redBox}>
-            <Link to="visuals">visuals</Link>
-          </div>
-          <div className={styles.greenBox}>
-            <Link to="shop">shop</Link>
-          </div>
-          <div className={styles.purpleBox}>
-            <Link to="about">about</Link>
+  
+  const Home = () => {
+    return(
+      <>
+    <Header />
+        <div className={styles.grad}>
+          <div className={styles.centered}>
+            <div className={styles.blueBox}>
+              <Link to="music">music</Link>
+            </div>
+            <div className={styles.redBox}>
+              <Link to="visuals">visuals</Link>
+            </div>
+            <div className={styles.greenBox}>
+              <Link to="shop">shop</Link>
+            </div>
+            <div className={styles.purpleBox}>
+              <Link to="about">about</Link>
+            </div>
           </div>
         </div>
-      </div>
-
+    <Footer />
+    </>
+    )
+  }
+  
+  return (
+    <>
+    <Router>
       <Switch>
-        <Route path="/music">
-          <Music />
-        </Route>
-        <Route path="/visuals">
-          <Visuals />
-        </Route>
-        <Route path="/shop">
-          <Shop />
-        </Route>
-        <Route path="/about">
-          <About />
-        </Route>
+        <Route path="/" exact component={Home} />
+        <Route path="/music" component={Music} />
+        <Route path="/About" component={About} />
+        <Route path="/shop" component={Shop} />
+        <Route path="/visuals" component={Visuals} />    
       </Switch>
-
-      </Router>
-      <Footer />
+    </Router>
     </>
   );
 }
+
+
 
 export default App;
