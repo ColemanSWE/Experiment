e08d39fbfe12649d8d57f54da8d1e43000ad3d59
Coleman Rose <coleman.rose@beamonpeople.se>
2020-04-08 19:30:42 +0200

    Moved the box on the homepage away from the hand icon. Added alt for header image.

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -6,7 +6,7 @@ export class Header extends Component {
         return (
             <div className={styles.header}>
                 <a className={styles.active} href="/"><img src="logotransparent.png" alt="Suffcation Pit" /></a>
-                <img className={styles.header} src="Wireframe Hand.png" />
+                <img className={styles.header} src="Wireframe Hand.png" alt="" />
                 <div className={styles.headerRight}>
                 </div>
             </div>
