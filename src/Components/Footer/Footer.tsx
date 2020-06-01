08fed6c7f6b4fef599f89c0fcbc6e840c0d90c4d
Coleman Rose <coleman.rose@beamonpeople.se>
2020-06-01 22:57:49 +0200

    Fixed an instagram logo fr footer.

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -8,6 +8,7 @@ export class Footer extends Component {
                 <a href="https://suffocationpit.bandcamp.com/"><img alt="bandcamp link" className={styles.bandcamp} src="bandcamp.png" /></a>
                 <a href="https://twitter.com/SuffocationPit"><img alt="twitter link" className={styles.twitter} src="twitter.png" /></a>
                 <a href="https://www.youtube.com/channel/UCaahR8RSBQxsGWTbT5bQACg"><img alt="youtube link" className={styles.youtube} src="youtube.png" /></a>
+                <a href="https://www.instagram.com/suffocation.pit/"><img alt="instagram link" className={styles.instagram} src="instagram.png" /></a>
             </div>
         )
     }
