f4e16fd350765bb751e1d08ac82a02c6b4398f3e
Coleman Rose <coleman.rose@beamonpeople.se>
2020-09-14 10:13:10 +0200

    Removed unneccessary elements from home and its css

--- src/Views/Home/Home.jsx
+++ src/Views/Home/Home.jsx
@@ -1,69 +1,21 @@
 import React, { Component } from "react";
 import styles from "./styles.module.scss";
-import { Link } from "react-router-dom";
-import Particles from "react-particles-js";
-import parameters from "../../particleConfig";
-import { Footer, Header } from "../../Components";
 
 export class Home extends Component {
   constructor(props) {
     super(props);
-    this.state = { width: 0, height: 0 };
-    this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
-  }
-
-  filler = "";
-  particles = undefined;
-
-  componentDidMount() {
-    this.updateWindowDimensions();
-    window.addEventListener("resize", this.updateWindowDimensions);
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener("resize", this.updateWindowDimensions);
-  }
-
-  updateWindowDimensions() {
-    this.setState({ width: window.innerWidth, height: window.innerHeight });
   }
 
   render() {
-    if (this.state.width >= 1100) {
-      this.filler = <br />;
-      this.particles = (
-        <Particles className={styles.particles} params={parameters} />
-      );
-    } else if (this.state.width < 1100) {
-      this.filler = "";
-    }
     return (
       <div className={styles.pageContainer}>
-        <Header />
-        <div className={styles.contentContainer}>
-          <img className={styles.hand} src="Wireframe Hand.png" alt="" />
-          <div className={styles.container}>
-            <a
-              href="https://soundcloud.com/suffocationpit"
-              className={styles.blueBox}
-            >
-              mus{this.filler}ic
-            </a>
-            <Link to="visuals" className={styles.redBox}>
-              visu{this.filler}als
-            </Link>
-            <a
-              href="https://teespring.com/stores/the-suffocation-pit"
-              className={styles.greenBox}
-            >
-              sh{this.filler}op
-            </a>
-            <Link to="about" className={styles.purpleBox}>
-              about/{this.filler}contact
-            </Link>
-          </div>
+        <div className={styles.logoContainer}>
+          <img
+            className={styles.logoAnimated}
+            src="logoblacknooutline.png"
+            alt="Suffcation Pit logo/home page button"
+          />
         </div>
-        <Footer />
       </div>
     );
   }
